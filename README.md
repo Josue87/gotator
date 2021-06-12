8998bad4ea29878dcefcc1479db0ea9458e5cde7
Josue87 <j.encinar.87@gmail.com>
2021-06-12 13:32:40 +0200

    Refactoring and extracting domains from the list

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Go version](https://img.shields.io/badge/go-v1.16-blue)](https://golang.org/dl/#stable)
 ![License](https://img.shields.io/badge/license-GNU-green.svg?style=square&logo=gnu)
-![Version](https://img.shields.io/badge/version-0.2b-yellow.svg?style=square)
+![Version](https://img.shields.io/badge/version-0.3b-yellow.svg?style=square)
 [![Author](https://img.shields.io/badge/author-@JosueEncinar-orange.svg?style=square&logo=twitter)](https://twitter.com/JosueEncinar)
 [![Tester](https://img.shields.io/badge/tester-@Six2dez1-orange.svg?style=square&logo=twitter)](https://twitter.com/six2dez1)
 
@@ -42,7 +42,8 @@ The options that can be used to launch the tool:
 * perm: List of permutations.
 * depth: Specify the depth (Between 1 and 3) - Default 1.
 * numbers: Specifies the number of iterations to the numbers found in the permutations (up and down). Default 0 Skip!
-* prefixes: Adding default gotator prefixes to permutations. If no perm list is specified, the default list is used. If perm is specified and this flag is present, the content is added to the permutations.
+* prefixes: Adding default gotator prefixes to permutations. If no perm list is specified, the default list is used. If perm is specified with this flag you merge the permutations.
+* md: Extract domains and subdomains from subdomains found in the list 'sub'
 
 Only the first option is mandatory.
 
@@ -55,7 +56,7 @@ gotator -sub domains.txt -perm permutations.txt -depth 2 -numbers 5 > output.txt
 To filter the result and remove possible duplicates:
 
 ```
-gotator -sub domains.txt -perm permutations.txt -depth 3 -numbers 10 | sort -u > output2.txt
+gotator -sub domains.txt -perm permutations.txt -depth 3 -numbers 10 -md | sort -u > output2.txt
 ```
 
 If you are compiling locally don't forget the ./ in front of your binary!
--- gotator.go
+++ gotator.go
@@ -22,7 +22,7 @@ func banner() {
 ▐                  █         ▐   ▐   █                  ▐     ▐   												   
 																																																		
 	> By @JosueEncinar
-	> Version 0.2b
+	> Version 0.3b
 	> Domain/subdomain permutator
 `
 	println(banner)
@@ -43,20 +43,8 @@ func containsElement(s []string, str string) bool {
 	return false
 }
 
-func permutatorNumbersAux(domain string, permutation string, numberToRlace string, intNumber int, permutations []string, depth int, delimitator string, permutatorNumber int) {
-	for i := 1; i <= permutatorNumber; i++ {
-		newSubdomain1 := strings.Replace(permutation, numberToRlace, strconv.Itoa(intNumber+i), -1) + delimitator + domain
-		fmt.Println(newSubdomain1)
-		permutator(newSubdomain1, permutations, depth-1, false, permutatorNumber)
-		if (intNumber - i) >= 0 {
-			newSubdomain2 := strings.Replace(permutation, numberToRlace, strconv.Itoa(intNumber-i), -1) + delimitator + domain
-			fmt.Println(newSubdomain2)
-			permutator(newSubdomain2, permutations, depth-1, false, permutatorNumber)
-		}
-	}
-}
-func permutatorNumbers(domain string, numbers []string, permutation string, joins []string,
-	permutations []string, depth int, firstTime bool, permutatorNumber int) {
+func permutatorWorker(tracker2 chan empty, domain string, permutationsChain chan string, depth int, firstTime bool, permutations []string) {
+	joins := []string{".", "-", ""}
 	allNumbers := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	for _, n := range allNumbers {
 		if strings.HasPrefix(domain, n) {
@@ -64,47 +52,21 @@ func permutatorNumbers(domain string, numbers []string, permutation string, join
 			break
 		}
 	}
-	defer func() {
-		recover()
-		for _, number := range numbers {
-			intNumber, err := strconv.Atoi(number)
-			if err != nil {
-				continue
-			}
-			if firstTime && !checkDomain(domain) {
-				joins = []string{"."}
-			}
-			for _, j := range joins {
-				permutatorNumbersAux(domain, permutation, number, intNumber, permutations, depth, j, permutatorNumber)
-			}
-		}
-	}()
-}
-
-func permutatorWorker(tracker2 chan empty, domain string, permutationsChain chan string, depth int, firstTime bool, iterateNumbers int, permutations []string) {
-	joins := []string{".", "-", ""}
-	pattern := regexp.MustCompile("\\d+")
 	for perm := range permutationsChain {
-		if iterateNumbers > 0 {
-			data := pattern.FindStringSubmatch(perm)
-			if len(data) > 0 {
-				permutatorNumbers(domain, data, perm, joins, permutations, depth, firstTime, iterateNumbers)
-			}
-		}
 		if firstTime && !checkDomain(domain) {
 			joins = []string{"."}
 		}
 		for _, j := range joins {
 			newSubDomain := perm + j + domain
 			fmt.Println(newSubDomain)
-			permutator(newSubDomain, permutations, depth-1, false, iterateNumbers)
+			permutator(newSubDomain, permutations, depth-1, false)
 		}
 	}
 	var e empty
 	tracker2 <- e
 }
 
-func permutator(domain string, permutations []string, depth int, firstTime bool, iterateNumbers int) {
+func permutator(domain string, permutations []string, depth int, firstTime bool) {
 	if depth < 1 {
 		return
 	}
@@ -116,7 +78,7 @@ func permutator(domain string, permutations []string, depth int, firstTime bool,
 	permutationsChain := make(chan string, threads)
 	tracker2 := make(chan empty)
 	for i := 0; i < threads; i++ {
-		go permutatorWorker(tracker2, domain, permutationsChain, depth, firstTime, iterateNumbers, permutations)
+		go permutatorWorker(tracker2, domain, permutationsChain, depth, firstTime, permutations)
 	}
 	for _, perm := range permutations {
 		permutationsChain <- fmt.Sprintf("%s", perm)
@@ -127,54 +89,29 @@ func permutator(domain string, permutations []string, depth int, firstTime bool,
 	}
 }
 
-func worker(tracker chan empty, subdomains chan string, permutations []string, depth int, iterateNumbers int) {
+func worker(tracker chan empty, subdomains chan string, permutations []string, depth int) {
 	for subdomain := range subdomains {
-		permutator(subdomain, permutations, depth, true, iterateNumbers)
+		permutator(subdomain, permutations, depth, true)
 	}
 	var e empty
 	tracker <- e
 }
 
-func main() {
-	prefixes := []string{"qa", "dev", "demo", "test", "prueba", "pre", "pro", "cuali", "www"}
-	threads := 20
-	var auxiliarDomains []string
-	var (
-		flDomains        = flag.String("sub", "", "List of domains to be swapped (1 per line)")
-		flPermutations   = flag.String("perm", "", "List of permutations (1 per line)")
-		flDepth          = flag.Int("depth", 1, "Specify the depth (Between 1 and 3)")
-		flIterateNumbers = flag.Int("numbers", 0, "Specifies the number of iterations to the numbers found in the permutations (up and down).")
-		flPrefixes       = flag.Bool("prefixes", false, "Adding gotator prefixes to permutations")
-	)
-	flag.Parse()
-
-	if *flDomains == "" {
-		fmt.Println("-sub is required")
-		os.Exit(1)
-	}
-
-	banner()
-	println("[i] Working in progress")
-
-	intDepth := *flDepth
-	if intDepth > 3 {
+func configureDepth(depth int) int {
+	auxDepth := depth
+	if depth > 3 {
 		println("[-] The maximum is 3. Configuring")
-		intDepth = 2
-	} else if intDepth < 1 {
+		auxDepth = 3
+	} else if depth < 1 {
 		println("[-] The minimum is 1. Configuring")
-		intDepth = 1
+		auxDepth = 1
 	}
+	return auxDepth
+}
 
-	if *flIterateNumbers < 0 {
-		println("[-] Numbers can not be < 0. Configuring to 0")
-		*flIterateNumbers = 0
-	}
-
-	domains := make(chan string, threads)
-	tracker := make(chan empty)
-	var permutations []string
-
-	fh1, err1 := os.Open(*flDomains)
+func generateDomains(flDomains string, flextractDomains bool) []string {
+	var auxiliarDomains []string
+	fh1, err1 := os.Open(flDomains)
 	if err1 != nil {
 		panic(err1)
 	}
@@ -182,27 +119,53 @@ func main() {
 
 	scanner1 := bufio.NewScanner(fh1)
 	for scanner1.Scan() {
-		auxiliarDomains = append(auxiliarDomains, fmt.Sprintf("%s", scanner1.Text()))
+		domain := fmt.Sprintf("%s", scanner1.Text())
+		auxiliarDomains = append(auxiliarDomains, domain)
+		if flextractDomains && checkDomain(domain) { //extract domain/subdomains from a subdomain
+			aux := strings.Split(domain, ".")
+			for {
+				if len(aux) < 2 {
+					break
+				}
+				aux2 := strings.Join(aux, ".")
+				if !containsElement(auxiliarDomains, aux2) {
+					auxiliarDomains = append(auxiliarDomains, aux2)
+				}
+				aux = strings.Split(aux2, ".")[1:]
+			}
+
+		}
 	}
-	if *flPermutations != "" {
-		fh2, err2 := os.Open(*flPermutations)
+	return auxiliarDomains
+}
+
+func generatePermutations(flPermutations string, flPrefixes bool, prefixes []string, permutatorNumber int, domains []string) []string {
+	var permutations []string
+	pattern := regexp.MustCompile("\\d+")
+	if flPermutations != "" {
+		fh2, err2 := os.Open(flPermutations)
 		if err2 != nil {
 			panic(err2)
 		}
 		defer fh2.Close()
 		scanner2 := bufio.NewScanner(fh2)
 		for scanner2.Scan() {
-			permutations = append(permutations, fmt.Sprintf("%s", scanner2.Text()))
+			line := fmt.Sprintf("%s", scanner2.Text())
+			permutations = append(permutations, line)
+			data := pattern.FindStringSubmatch(line)
+			if len(data) > 0 && permutatorNumber > 0 {
+				permutatorNumbers(&permutations, line, data, permutatorNumber)
+			}
 		}
 	}
-	if *flPrefixes || *flPermutations == "" {
+	if flPrefixes || flPermutations == "" {
 		for _, prefix := range prefixes {
 			if !containsElement(permutations, prefix) {
 				permutations = append(permutations, prefix)
 			}
 		}
 	}
-	for _, dom := range auxiliarDomains {
+	for _, dom := range domains {
 		if checkDomain(dom) {
 			aux := strings.Split(dom, ".")
 			total := len(aux) - 2
@@ -214,9 +177,64 @@ func main() {
 			}
 		}
 	}
+	return permutations
+}
+
+func permutatorNumbers(permutations *[]string, permutation string, dataToReplace []string, permutatorNumber int) {
+	defer func() {
+		recover()
+		for _, numberToRlace := range dataToReplace {
+			intNumber, err := strconv.Atoi(numberToRlace)
+			if err != nil {
+				continue
+			}
+			for i := 1; i <= permutatorNumber; i++ {
+				*permutations = append(*permutations, strings.Replace(permutation, numberToRlace, strconv.Itoa(intNumber+i), -1))
+				if (intNumber - i) >= 0 {
+					*permutations = append(*permutations, strings.Replace(permutation, numberToRlace, strconv.Itoa(intNumber-i), -1))
+				}
+			}
+		}
+
+	}()
+}
+
+func main() {
+	prefixes := []string{"qa", "dev", "demo", "test", "prueba", "pre", "pro", "cuali", "www"}
+	threads := 10
+	var (
+		flDomains        = flag.String("sub", "", "List of domains to be swapped (1 per line)")
+		flPermutations   = flag.String("perm", "", "List of permutations (1 per line)")
+		flDepth          = flag.Int("depth", 1, "Specify the depth (Between 1 and 3)")
+		flIterateNumbers = flag.Int("numbers", 0, "Permute the numbers found in the list of permutations")
+		flPrefixes       = flag.Bool("prefixes", false, "Adding gotator prefixes to permutations")
+		flextractDomains = flag.Bool("md", false, "Extract domains and subdomains from subdomains found in the list 'sub'")
+	)
+	flag.Parse()
+
+	if *flDomains == "" {
+		fmt.Println("-sub is required")
+		os.Exit(1)
+	}
+
+	banner()
+	println("[i] Working in progress")
+
+	intDepth := configureDepth(*flDepth)
+
+	if *flIterateNumbers < 0 {
+		println("[-] Numbers can not be < 0. Configuring to 0")
+		*flIterateNumbers = 0
+	}
+
+	domains := make(chan string, threads)
+	tracker := make(chan empty)
+
+	auxiliarDomains := generateDomains(*flDomains, *flextractDomains)
+	permutations := generatePermutations(*flPermutations, *flPrefixes, prefixes, *flIterateNumbers, auxiliarDomains)
 
 	for i := 0; i < threads; i++ {
-		go worker(tracker, domains, permutations, intDepth, *flIterateNumbers)
+		go worker(tracker, domains, permutations, intDepth)
 	}
 
 	for _, dom := range auxiliarDomains {
